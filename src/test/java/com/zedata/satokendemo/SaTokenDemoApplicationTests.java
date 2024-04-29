a10f089ee788e6ae9e492d63794c7d7ccf9fe056
Mr-Glacier <K15100415112@163.com>
2024-04-29 11:02:43 +0800

    Initial commit

---
+++ src/test/java/com/zedata/satokendemo/SaTokenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zedata.satokendemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaTokenDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
